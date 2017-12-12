2793b8408d6e97da023636763ea5d20b7f799c48
Daniel França <daniel.franca@protonmail.com>
2017-12-12 10:13:39 +0100

    Changed setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
-from distutils.core import setup
-from pip.req import parse_requirements
-
-install_reqs = parse_requirements('requirements.txt', session='hack')
-
-reqs = [str(ir.req) for ir in install_reqs]
-
-setup(name='feel_auth',
-    version='0.1',
-    py_modules=['feel_auth'],
-    install_reqs=reqs
+from setuptools import setup, find_packages
+setup(
+    name="FeelAuth",
+    version="0.1",
+    packages=find_packages(),
 )
